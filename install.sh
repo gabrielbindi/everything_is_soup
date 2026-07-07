a5cbd582bfec5dac049b03e31cd4d38110ffaf6b
gabrielbindi <binderbauer.gabriel@gmail.com>
2026-07-07 15:08:50 +0200

    jojojo

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ TARGET_DIR="$HOME/.everything_is_soup"
 command mkdir -p "$TARGET_DIR/plugins"
 
 command echo "[+] Installing soup-kitchen..."
-URL_CONFIG="https://raw.githubusercontent.com/gabrielbindi/everything_is_soup/shell/soup.config"
+URL_CONFIG="https://raw.githubusercontent.com/gabrielbindi/everything_is_soup/docker/soup.config"
 command curl -sSL "$URL_CONFIG" -o "$TARGET_DIR/soup.config"
-URL_LAZYGIT="https://raw.githubusercontent.com/gabrielbindi/everything_is_soup/shell/plugins/lazygit.sh"
+URL_LAZYGIT="https://raw.githubusercontent.com/gabrielbindi/everything_is_soup/docker/plugins/lazygit.sh"
 command curl -sSL "$URL_LAZYGIT" -o "$TARGET_DIR/plugins/lazygit.sh"
+URL_LAZYDOCKER="https://raw.githubusercontent.com/gabrielbindi/everything_is_soup/docker/plugins/lazydocker.sh"
+command curl -sSL "$URL_LAZYDOCKER" -o "$TARGET_DIR/plugins/l
 
 if ! grep -q "soup.config" "$HOME/.bashrc"; then
     command echo "[+] Configuring soup-kitchen as your shell environment..."
