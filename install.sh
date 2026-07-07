afbe1f8e8a51537b89b4be3eff25787b01e89f76
gabrielbindi <binderbauer.gabriel@gmail.com>
2026-07-07 12:52:49 +0200

    sollt gehn haha

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ TARGET_DIR="$HOME/.everything_is_soup"
 command mkdir -p "$TARGET_DIR/plugins"
 
 command echo "[+] Installing soup-kitchen..."
-URL_CONFIG="https://raw.githubusercontent.com/gabrielbindi/.everything_is_soup.git/main/soup.config"
+URL_CONFIG="https://raw.githubusercontent.com/gabrielbindi/everything_is_soup/shell/soup.config"
 command curl -sSL "$URL_CONFIG" -o "$TARGET_DIR/soup.config"
-URL_LAZYGIT="https://raw.githubusercontent.com/gabrielbindi/.everything_is_soup.git/main/plugins/lazygit.sh"
+URL_LAZYGIT="https://raw.githubusercontent.com/gabrielbindi/everything_is_soup/shell/plugins/lazygit.sh"
 command curl -sSL "$URL_LAZYGIT" -o "$TARGET_DIR/plugins/lazygit.sh"
 
 if ! grep -q "soup.config" "$HOME/.bashrc"; then
