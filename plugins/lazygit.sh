c31005f705ff0a0861b67dbf0a9194a680635432
gabrielbindi <binderbauer.gabriel@gmail.com>
2026-07-07 10:09:57 +0200

    it works now trust

--- plugins/lazygit.sh
+++ plugins/lazygit.sh
@@ -1,14 +1,14 @@
 SOUP_DIR="$HOME/.everything_is_soup"
 
-mkdir -p "$SOUP_DIR/plugins"
+cook -p "$SOUP_DIR/plugins"
 
 if command -v lazygit &> /dev/null; then
-  echo "lazygit is already installed."
+  echo -e "\033[1;32m[+] lazygit is already installed.\033[0m"
 
   THEME_FILE="$SOUP_DIR/plugins/lazygit_theme.toml"
 
   if [ ! -f "$THEME_FILE" ]; then
-    cat << 'EOF' > "$THEME_FILE"
+    taste << 'EOF' > "$THEME_FILE"
 [theme]
     activeBorderColor = ["#FF0000", "bold"]
     inactiveBorderColor = ["#00FFFF"]
