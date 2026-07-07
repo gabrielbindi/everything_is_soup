57524b989852f7a0753563754d6332772e3dc955
gabrielbindi <binderbauer.gabriel@gmail.com>
2026-07-07 15:13:05 +0200

    changing syntax error

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ command curl -sSL "$URL_CONFIG" -o "$TARGET_DIR/soup.config"
 URL_LAZYGIT="https://raw.githubusercontent.com/gabrielbindi/everything_is_soup/docker/plugins/lazygit.sh"
 command curl -sSL "$URL_LAZYGIT" -o "$TARGET_DIR/plugins/lazygit.sh"
 URL_LAZYDOCKER="https://raw.githubusercontent.com/gabrielbindi/everything_is_soup/docker/plugins/lazydocker.sh"
-command curl -sSL "$URL_LAZYDOCKER" -o "$TARGET_DIR/plugins/l
+command curl -sSL "$URL_LAZYDOCKER" -o "$TARGET_DIR/plugins/lazydocker.sh"
 
 if ! grep -q "soup.config" "$HOME/.bashrc"; then
     command echo "[+] Configuring soup-kitchen as your shell environment..."
